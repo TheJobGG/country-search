fae98a65473e643e3fa01eff64c7cd1d68a65915
TheJobGG <jobglez80@gmail.com>
2023-06-16 15:39:23 -0600

    Add sun icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,9 +1,9 @@
-interface IconMoonProps {
+interface classNameProp {
   className?: string;
 }
 
 
-export function IconMoon({ className }: IconMoonProps) {
+export function IconMoon({ className }: classNameProp) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" className={className}>
       <path d="M145 99a224 224 0 0 0 117 329A175 175 0 0 1 48 256c0-69 39-128 97-157zm62-66a224 224 0 1 0 145 406l5-4a216 216 0 0 0 22-18 16 16 0 0 0-14-27l-11 1-15 1h-4A176 176 0 0 1 224 80l3-2a158 158 0 0 1 22-15 16 16 0 0 0-7-30l-10-1h-18l-7 1z" />
@@ -11,26 +11,35 @@ export function IconMoon({ className }: IconMoonProps) {
   )
 }
 
-export function IconBackArrow({ className }: IconMoonProps) {
+export function IconBackArrow({ className }: classNameProp) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" className={className}>
-      <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z"/>
+      <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z" />
     </svg>
   )
 }
 
-export function IconSearch({ className }: IconMoonProps) {
-  return(
+export function IconSearch({ className }: classNameProp) {
+  return (
     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" className={className}>
-      <path d="M416 208c0 46-15 88-40 123l127 126a32 32 0 0 1-46 46L331 376a208 208 0 1 1 85-168zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+      <path d="M416 208c0 46-15 88-40 123l127 126a32 32 0 0 1-46 46L331 376a208 208 0 1 1 85-168zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
     </svg>
   )
 }
 
-export function IconChevronDown({ className }: IconMoonProps) {
-  return(
+export function IconChevronDown({ className }: classNameProp) {
+  return (
     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" className={className}>
-      <path d="M233 407c13 12 33 12 46 0l192-192a32 32 0 0 0-46-46L256 339 87 169a32 32 0 0 0-46 46l192 192z"/>
+      <path d="M233 407c13 12 33 12 46 0l192-192a32 32 0 0 0-46-46L256 339 87 169a32 32 0 0 0-46 46l192 192z" />
+    </svg>
+  )
+}
+
+
+export function IconSun({ className }: classNameProp) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" className={className}>
+      <path d="M362 1c5 2 8 7 9 12l20 108 108 20c5 1 10 4 12 9s1 11-2 16l-62 90 62 90c3 5 4 11 2 16s-7 8-12 9l-108 20-20 108c-1 5-4 10-9 12s-11 1-16-2l-90-62-90 62c-5 3-11 4-16 2s-8-7-9-12l-20-108-108-20c-5-1-10-4-12-9s-1-11 2-16l62-90-62-90c-3-5-4-11-2-16s7-8 12-9l108-20 20-108c1-5 5-10 10-12s10-1 15 2l90 62 90-62c5-3 11-4 16-2zM160 256a96 96 0 1 1 192 0 96 96 0 1 1-192 0zm224 0a128 128 0 1 0-256 0 128 128 0 1 0 256 0z" />
     </svg>
   )
 }
